fd46ea5cfe8662bfdf563f260cadf07426f6caf1
agenthun <hun333@126.com>
2015-07-02 18:16:05 +0800

    v1.1

--- lotusleaf_refresh/src/main/java/com/agenthun/lotusleaf/activity/MainActivity.java
+++ lotusleaf_refresh/src/main/java/com/agenthun/lotusleaf/activity/MainActivity.java
@@ -1,47 +1,44 @@
 package com.agenthun.lotusleaf.activity;
 
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.agenthun.lotusleaf.R;
 import com.agenthun.lotusleaf.UiUtils;
 import com.agenthun.lotusleaf.view.LotusLeafSunHeaderView;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import in.srain.cube.image.CubeImageView;
-import in.srain.cube.image.ImageLoader;
-import in.srain.cube.image.ImageLoaderFactory;
-import in.srain.cube.image.ImageTask;
-import in.srain.cube.image.iface.ImageLoadHandler;
 import in.srain.cube.views.ptr.PtrFrameLayout;
 import in.srain.cube.views.ptr.PtrHandler;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mUrl = "http://www.hdwallpapers.in/download/poltergeist_2015_movie-1280x720.jpg";
-    private long mStartLoadingTime = -1;
-    private boolean mImageHasLoaded = false;
+    private List<String> mUrls;
 
-    @InjectView(R.id.material_style_image_view)
-    CubeImageView imageView;
     @InjectView(R.id.material_style_ptr_frame)
     PtrFrameLayout frameLayout;
+    @InjectView(R.id.linearlayout)
+    LinearLayout linearLayout;
+    @InjectView(R.id.image_view)
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-
-        final ImageLoader imageLoader = ImageLoaderFactory.create(this);
+        mUrls = Arrays.asList(getResources().getStringArray(R.array.images_url));
 
         LotusLeafSunHeaderView headerView = new LotusLeafSunHeaderView(this);
         headerView.setLayoutParams(new PtrFrameLayout.LayoutParams(-1, -2));
@@ -67,49 +64,38 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onRefreshBegin(PtrFrameLayout ptrFrameLayout) {
-                if (mImageHasLoaded) {
-                    long delay = 1500;
-                    frameLayout.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            frameLayout.refreshComplete();
-                        }
-                    }, delay);
-                } else {
-                    mStartLoadingTime = System.currentTimeMillis();
-                    imageView.loadImage(imageLoader, mUrl);
-                }
+                updateImage();
             }
         });
+    }
 
-        ImageLoadHandler imageLoadHandler = new ImageLoadHandler() {
+    private void updateImage() {
+        frameLayout.postDelayed(new Runnable() {
             @Override
-            public void onLoading(ImageTask imageTask, CubeImageView cubeImageView) {
+            public void run() {
+                Picasso.with(MainActivity.this)
+                        .load(mUrls.get((int) (Math.random() * mUrls.size())))
+                        .into(imageView, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                linearLayout.setScaleX(0);
+                                linearLayout.setScaleY(0);
+                                linearLayout.animate()
+                                        .scaleX(1)
+                                        .scaleY(1)
+                                        .setDuration(200)
+                                        .setInterpolator(new DecelerateInterpolator())
+                                        .setStartDelay(100)
+                                        .start();
+                            }
 
+                            @Override
+                            public void onError() {
+
+                            }
+                        });
+                frameLayout.refreshComplete();
             }
-
-            @Override
-            public void onLoadFinish(ImageTask imageTask, final CubeImageView cubeImageView, final BitmapDrawable bitmapDrawable) {
-                mImageHasLoaded = true;
-                long delay = 1500;
-                frameLayout.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (cubeImageView != null && bitmapDrawable != null) {
-                            TransitionDrawable wl = new TransitionDrawable(new Drawable[]{new ColorDrawable(Color.WHITE), (Drawable) bitmapDrawable});
-                            imageView.setImageDrawable(wl);
-                            wl.startTransition(200);
-                        }
-                        frameLayout.refreshComplete();
-                    }
-                }, delay);
-            }
-
-            @Override
-            public void onLoadError(ImageTask imageTask, CubeImageView cubeImageView, int i) {
-
-            }
-        };
-        imageLoader.setImageLoadHandler(imageLoadHandler);
+        }, 1500);
     }
 }
